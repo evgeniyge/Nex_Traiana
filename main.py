1c91684322a913e8ebbb68ccbb9720d0d968ef4f
Evgeniy German <evgeniyge@gmail.com>
2017-05-12 22:36:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+from browser import browser
+
+
+
+
+    
+def main():
+    obj = browser()
+    if (obj == False):
+        sys.exit(1)
+    obj.openUrl()
+    obj.search()
+    if(obj.getResultsNum() < obj.getActualResultsNum()):
+        print ("Actual results num %d, expected %d" % (obj.resultNum, obj.actualResultsNum))
+
+    nameList = obj.getItemsWithoutRating()
+    if (nameList):
+        print ("Items without rating: ")
+        for i in range (len(nameList)):
+            print ("%d - %s " % (i + 1, nameList[i]) )
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
